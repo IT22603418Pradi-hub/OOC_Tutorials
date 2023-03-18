55af4ade6089067d437c22d0cef753b590055b0d
Pradicksha <121156636+Pradicksha@users.noreply.github.com>
2023-03-18 16:21:12 +0000

    tute-02 ex-3 done

--- .ccls-cache/@home@runner@tutorial-02/ex2.cpp
+++ .ccls-cache/@home@runner@tutorial-02/ex2.cpp
@@ -7,22 +7,25 @@ using namespace std;
 int main(void) {
   double salary, netSalary;
   int etype, otHrs, otRate;
-  
+
   cout << "Enter Employee Type : ";
   cin >> etype;
   cout << "Enter Salary : ";
   cin >> salary;
   cout << "Enter OtHrs : ";
-  cin >> otHrs; 
-  
+  cin >> otHrs;
+
   switch (etype) {
-    case 1 : otRate = 1000;
-      break;
-    case 2 : otRate = 1500;
-      break;
-    default : otRate = 1700;
-      break;
+  case 1:
+    otRate = 1000;
+    break;
+  case 2:
+    otRate = 1500;
+    break;
+  default:
+    otRate = 1700;
+    break;
   }
-  netSalary = salary + otHrs* otRate;
+  netSalary = salary + otHrs * otRate;
   cout << "Net Salary is " << netSalary;
-} 
+}
